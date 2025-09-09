8ef5bcf0f312cf3cd326f64f5b7f5c43aa407197
agiratech-shobana <shobana.m@agiratech.com>
2025-09-09 16:46:21 +0530

    frontend change

--- backend/server.js
+++ backend/server.js
@@ -80,6 +80,10 @@ app.use((err, req, res, next) => {
 });
 
 
+app.get("/", (req, res) => {
+  res.send("Server is running! Welcome to RMG backend.");
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
